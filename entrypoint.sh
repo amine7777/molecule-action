e9201adf819996174d009814faec2268d4e246c4
Amine Kahlaoui <amine.kahlaoui@iec.ch>
2020-11-11 22:46:34 +0100

    Edit action

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-apk add build-base libffi-dev musl-dev openssl-dev
-apk add python3-dev py-pip
-apk add ansible
-pip3 install --upgrade pip
-pip3 install docker 
-pip3 install molecule[docker] yamllint ansible-lint

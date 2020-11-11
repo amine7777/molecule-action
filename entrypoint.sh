2f06090b4ef7eee8809e563dff462cf7a44a7459
Amine Kahlaoui <amine.kahlaoui@iec.ch>
2020-11-11 19:37:52 +0100

    Edit action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 apk add build-base libffi-dev musl-dev openssl-dev
 apk add python3-dev py-pip
-pip3 install docker ansible
+apk add ansible
+pip3 install --upgrade pip
+pip3 install docker 
 pip3 install molecule[docker] yamllint ansible-lint
